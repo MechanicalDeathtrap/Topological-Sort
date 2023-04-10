7a305093ab75a6378cba820b5cee34e939c499b3
MechanicalDeathtrap <korikova-04@mail.ru>
2023-04-10 18:17:51 +0300

    Add files via upload

---
+++ Graph.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Topological_Sort
+{
+    internal class Graph
+    {
+        private int vertexes;
+        private List<List<int>> adjacency;
+
+        public Stopwatch time = new Stopwatch();
+        int iteration = 0;
+
+        public Graph(int Vertexes)
+        {
+            vertexes = Vertexes;
+            adjacency = new List<List<int>>();
+            for (int i = 0; i < Vertexes; i++)
+                adjacency.Add(new List<int>());
+        }
+
+        public void AddEdge(int u, int v) => adjacency[u].Add(v); 
+
+        private void MainTopologicalSort( Stack<int> stack, int vertexes, bool[] isVertexVisited )
+        {
+            isVertexVisited[vertexes] = true;
+
+            foreach (var vertex in adjacency[vertexes])
+            {
+                iteration++;
+                if (!isVertexVisited[vertex]) MainTopologicalSort(stack, vertex, isVertexVisited);
+            }
+
+            stack.Push(vertexes);
+        }
+
+        public Stack<int> TopologicalSort(out int timef, out int iterations)
+        {
+            time.Start();
+            var stack = new Stack<int>();
+            var visited = new bool[vertexes];
+
+            for (int i = 0; i < vertexes; i++)
+            {
+                iteration++;
+                if (!visited[i]) MainTopologicalSort(stack, i, visited);
+            }
+
+            time.Stop();
+            timef = (int)time.ElapsedTicks;
+            iterations = iteration;
+            return stack;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Topological_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TopologicalSortForFile("graphs36.txt", "output36.txt", "info36.txt");
+        }
+        public static void TopologicalSortForFile(string _input, string _output, string _info)
+        {
+            StreamReader input = new StreamReader(_input);
+            StreamWriter output = new StreamWriter(_output);
+            StreamWriter info = new StreamWriter(_info);
+
+            int time;
+            int iterations;
+            string[] file;
+            info.WriteLine(" Time " + "  " + " Iterations ");
+
+            while (!input.EndOfStream)
+            {
+                file = input.ReadLine().Split('-');
+                Graph graph = new Graph(int.Parse(file[0]));
+
+                for (int i = 1; i < file.Length; i++)
+                {
+                    var fil = file[i].Replace('[', ' ').Replace(']', ' ').Trim();
+                    var vertexes = fil.Split(new string[] { ", " }, StringSplitOptions.None);
+                    foreach (var vertex in vertexes)
+                    {
+                        Console.WriteLine(vertex);
+                    }
+                    graph.AddEdge(int.Parse(vertexes[0]), int.Parse(vertexes[1]));
+                }
+
+                var sorted = graph.TopologicalSort(out time, out iterations).ToArray();
+
+                info.WriteLine(time + " , " + iterations);
+
+                output.Write('{');
+                for (int i = 0; i < sorted.Length; i++)
+                {
+                    if (i + 1 == sorted.Length)
+                        output.Write(sorted[i]);
+                    else
+                        output.Write(sorted[i] + " , ");
+                }
+
+                output.Write('}');
+                output.WriteLine();
+
+            }
+            output.Close();
+            input.Close();
+            info.Close();
+
+        }
+    }
+}
